e13a06e2868006ef372dbeba4571e8f88a86ae3b
Tobias Schlitt <toby@php.net>
2005-05-23 11:49:58 +0000

    * Added tests for SpamCheck modules. * Outsourced trackback data to trackback_data.php for reuse in unit test.
    git-svn-id: http://svn.php.net/repository/pear/packages/Services_Trackback/trunk@186853 c90b9560-bf6c-de11-be94-00142212c4b1

--- test/simple_test.php
+++ test/simple_test.php
@@ -2,57 +2,133 @@
 
 set_include_path('.:/cvs/pear/Services_Trackback:/usr/php4/share/pear');
 
-require_once 'Services/Trackback.php';
-
-$trackback = new Services_Trackback(array('id' => 1));
-
-echo "\n\n--------------- Success response ---------------\n\n";
-echo $trackback->getResponseSuccess();
-
-echo "\n\n--------------- Error response ---------------\n\n";
-echo $trackback->getResponseError(1, "Test error");
-
-$trackback = Services_Trackback::create(array('id' => 1, 'title' => 'Test title', 'url' => 'http://www.example.com/', 'trackback_url' => 'http://www.example.com/trackback/1'));
-
-echo "\n\n--------------- Newly created trackback object ---------------\n\n";
-
-var_dump($trackback);
-
-echo "\n\n--------------- Autodiscovery code (with comments) ---------------\n\n";
-var_dump($trackback->getAutoDiscoveryCode());
-
-echo "\n\n--------------- Autodiscovery code (without comments) ---------------\n\n";
-var_dump($trackback->getAutoDiscoveryCode(false));
-
-$data = array(
-    'title'     => 'Test title',
-    'excerpt'   => 'A test blog entry',
-    'url'       => 'http://www.example.com/',
-    'blog_name' => 'Example weblog'
+$conf = array(
+    'online' => true,
 );
 
-$trackback = Services_Trackback::create(array('id' => 1));
-var_dump($trackback->receive($data));
+// {{{ Var_Dump
 
-echo "\n\n--------------- Received trackback object ---------------\n\n";
-var_dump($trackback);
+// For more readable dumping of variables
+require_once 'Var_Dump.php';
 
+if (isset($_SERVER['REQUEST_URI'])) {
+    // Setup for displaying XHTML output.
+    Var_Dump::displayInit(
+        array('display_mode'=>'XHTML_Text'), 
+        array(
+            'mode'          => 'wide',
+            'offset'        => 4,
+            'after_text'    => '<br />',
+        )
+    );
+    // Headline function for XHTML output.
+    function head ( $text ) {
+        echo '<br /><b>'.$text.'</b><br />';
+    }
+} else {
+    // Setup for displaying console output.
+    Var_Dump::displayInit(
+        // Choose text mode
+        array('display_mode'    =>  'Text'),
+        // Rendere options
+        array(
+            'mode'              =>  'wide',
+            'before_text'       =>  "\n",
+            'after_text'        =>  "\n",
+        )
+    );
+    // Headline function for Text output.
+    function head ( $text ) {
+        echo "\n\n--- ".$text." ---\n\n";
+    }
+}
 
-echo "\n\n--------------- Autodiscovered trackback object ---------------\n\n";
+// }}}
 
-$trackback = Services_Trackback::create(array('id' => 'Test', 'url' => 'http://pear.php.net/package/Net_FTP'));
-var_dump($trackback->autodiscover());
+require_once 'Services/Trackback.php';
+require_once 'test/trackback_data.php';
 
-var_dump($trackback);
+$trackback = new Services_Trackback($trackbackData['nospam']);
 
-echo "\n\n--------------- Sending trackback ---------------\n\n";
+head('Success response');
+Var_Dump::display($trackback->getResponseSuccess());
 
-$trackback->set('title', 'Testing Services_Trackback');
-$trackback->set('url', 'http://www.example.com');
-$trackback->set('excerpt', 'Test test tes...');
-$trackback->set('blog_name', 'Tobias Schlitt testing Services_Trackback');
+head('Error response');
+Var_Dump::display($trackback->getResponseError(1, "Test error"));
 
-var_dump($trackback);
-var_dump($trackback->send());
+unset($trackback);
 
+$trackback = Services_Trackback::create($trackbackData['nospam']);
+
+head('Newly created trackback object');
+
+Var_Dump::display($trackback);
+
+head('Autodiscovery code (with comments)');
+Var_Dump::display($trackback->getAutoDiscoveryCode());
+
+head('Autodiscovery code (without comments)');
+Var_Dump::display($trackback->getAutoDiscoveryCode(false));
+
+unset($trackback);
+
+$trackback = Services_Trackback::create($trackbackData['nospam']);
+Var_Dump::display($trackback->receive($trackbackData['undetected']));
+
+head('Received trackback object');
+Var_Dump::display($trackback);
+
+unset($trackback);
+
+if (true === $conf['online']) {
+    head('Autodiscovered trackback object');
+
+    $trackback = Services_Trackback::create(array('id' => 'Test', 'url' => 'http://pear.php.net/package/Net_FTP'));
+    Var_Dump::display($trackback->autodiscover());
+    Var_Dump::display($trackback);
+
+    head('Sending trackback');
+
+    $trackback->set('title', 'Testing Services_Trackback');
+    $trackback->set('url', 'http://www.example.com');
+    $trackback->set('excerpt', 'Test test tes...');
+    $trackback->set('blog_name', 'Tobias Schlitt testing Services_Trackback');
+
+    Var_Dump::display($trackback);
+    Var_Dump::display($trackback->send());
+
+    unset($trackback);
+
+	head('Wordlist spam check');
+	
+	foreach ($trackbackData as $id =>  $set) {
+	    echo "\n\n-- $id --";
+	    $trackback = Services_Trackback::create($set);
+	    $trackback->createSpamCheck('Wordlist');
+	    Var_Dump::display($trackback->checkSpam());
+	    // Var_Dump::display($trackback);
+	}
+	
+	head('DNSBL spam check');
+	
+	foreach ($trackbackData as $id =>  $set) {
+	    echo "\n\n-- $id --";
+	    $trackback = Services_Trackback::create($set);
+	    $trackback->createSpamCheck('DNSBL');
+	    Var_Dump::display($trackback->checkSpam());
+	    // Var_Dump::display($trackback);
+	}
+	
+	head('SURBL spam check');
+	
+	foreach ($trackbackData as $id =>  $set) {
+	    echo "\n\n-- $id --";
+	    $trackback = Services_Trackback::create($set);
+	    $trackback->createSpamCheck('SURBL');
+	    Var_Dump::display($trackback->checkSpam());
+	    // Var_Dump::display($trackback);
+	}
+
+}
+    	
 ?>
