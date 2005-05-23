32cd153b1b54eb5d0dca61caf37b8275dc0dee2c
Tobias Schlitt <toby@php.net>
2005-05-23 14:26:25 +0000

    * Added unit test for Regex SpamCheck.
    git-svn-id: http://svn.php.net/repository/pear/packages/Services_Trackback/trunk@186871 c90b9560-bf6c-de11-be94-00142212c4b1

--- test/Services_Trackback_TestAll.php
+++ test/Services_Trackback_TestAll.php
@@ -8,6 +8,8 @@ require_once 'Services_Trackback_SpamCheck_Test.php';
 
 echo "\n\nRunning test suite for Services_Trackback_SpamCheck_DNSBL\n\n";
 require_once 'Services_Trackback_SpamCheck_DNSBL_Test.php';
+echo "\n\nRunning test suite for Services_Trackback_SpamCheck_Regex\n\n";
+require_once 'Services_Trackback_SpamCheck_Regex_Test.php';
 echo "\n\nRunning test suite for Services_Trackback_SpamCheck_SURBL\n\n";
 require_once 'Services_Trackback_SpamCheck_SURBL_Test.php';
 echo "\n\nRunning test suite for Services_Trackback_SpamCheck_Wordlist\n\n";
