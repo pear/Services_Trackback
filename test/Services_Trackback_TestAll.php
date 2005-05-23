ebee589c177b844ee9d35ad1a66628631ec78df8
Tobias Schlitt <toby@php.net>
2005-05-23 14:07:42 +0000

    * Run all test suites.
    git-svn-id: http://svn.php.net/repository/pear/packages/Services_Trackback/trunk@186867 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ test/Services_Trackback_TestAll.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "\n\nRunning test suite for Services_Trackback\n\n";
+require_once 'Services_Trackback_Test.php';
+
+echo "\n\nRunning test suite for Services_Trackback_SpamCheck\n\n";
+require_once 'Services_Trackback_SpamCheck_Test.php';
+
+echo "\n\nRunning test suite for Services_Trackback_SpamCheck_DNSBL\n\n";
+require_once 'Services_Trackback_SpamCheck_DNSBL_Test.php';
+echo "\n\nRunning test suite for Services_Trackback_SpamCheck_SURBL\n\n";
+require_once 'Services_Trackback_SpamCheck_SURBL_Test.php';
+echo "\n\nRunning test suite for Services_Trackback_SpamCheck_Wordlist\n\n";
+require_once 'Services_Trackback_SpamCheck_Wordlist_Test.php';
+
+?>
